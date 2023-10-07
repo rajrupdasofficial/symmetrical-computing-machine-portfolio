c80346d3f3622eb7fcd57f2b7fabcc2b4e79e4a6
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-07 08:35:59 +0530

    Blogs sections is working
    Blogs section is working and able to fetch data from website
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 import "../assets/css/blogpage.scss";
 import NavBar from "./Nav";
 import Layout from "./Layout";
@@ -12,134 +13,78 @@ export interface BlogPost {
 const Blog: React.FC = () => {
   const [blogPosts, setBlogPosts] = useState<BlogPost[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const apiURl = import.meta.env.VITE_REACT_APP_API_URL;
+  const apk = import.meta.env.VITE_REACT_APP_API_KEY;
 
-  // Infinite scroll logic
+  const apl = import.meta.env.VITE_REACT_APP_PARAMS;
+
+  const fetchBlogPosts = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get(apiURl, {
+        headers: {
+          Authorization: `${apl} ${apk}`,
+        },
+      });
+      const newPosts = response.data;
+      setBlogPosts((prevPosts) => [...prevPosts, ...newPosts]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Display an error message to the user
+    } finally {
+      setIsLoading(false);
+    }
+  }, [apiURl, apk, apl]);
+
   const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
-
     if (scrollHeight - scrollTop === clientHeight && !isLoading) {
-      // Fetch more blog posts here (similar to the useEffect below)
-      setIsLoading(true);
-      setTimeout(() => {
-        // Simulated new data
-        const newPosts = [
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-          { id: 3, title: "Post 3", content: "Content of Post 3" },
-          { id: 4, title: "Post 4", content: "Content of Post 4" },
-        ];
-        setBlogPosts((prevPosts) => [...prevPosts, ...newPosts]);
-        setIsLoading(false);
-      }, 1000);
+      fetchBlogPosts();
     }
   };
 
-  // Simulate fetching blog posts (you can replace this with actual API calls)
   useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      // Example data (you can replace this with actual data)
-      const newPosts = [
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-        { id: 3, title: "Post 3", content: "Content of Post 3" },
-        { id: 4, title: "Post 4", content: "Content of Post 4" },
-      ];
-      setBlogPosts((prevPosts) => [...prevPosts, ...newPosts]);
-      setIsLoading(false);
-    }, 1000); // Simulated delay
-  }, []); // Empty dependency array to run only once when the component mounts
+    fetchBlogPosts();
+  }, [fetchBlogPosts]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000); // Simulate a 5-second loading time (adjust as needed)
-  }, []);
   return (
-    <>
-      {loading ? (
-        <div className="container">
-          <div className="progress progress-striped">
-            <div className="progress-bar"></div>
+    <Layout title="blogs | read latest blogs and trending news free">
+      <NavBar />
+      <div className="blog-page">
+        <div className="blog-left" onScroll={handleScroll}>
+          {blogPosts.map((post) => (
+            <div className="blog-card" key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </div>
+          ))}
+          {isLoading && <p>Loading...</p>}
+        </div>
+        <div className="blog-right">
+          <div className="recent-posts">
+            <h3>Recent Posts</h3>
+            <ul>
+              {blogPosts.slice(0, 10).map((post) => (
+                <li key={post.id}>
+                  <a href="#">{post.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="popular-topics">
+            <h3>Popular Topics</h3>
+            <ul>
+              {/* Render up to 4 popular topics here */}
+              {/* ... */}
+            </ul>
+          </div>
+          <div className="search">
+            <input type="text" placeholder="Search..." />
+            <button>Search</button>
           </div>
         </div>
-      ) : (
-        <Layout title="blogs | read latest blogs and trending news free">
-          <NavBar />
-          <div className="blog-page">
-            <div className="blog-left" onScroll={handleScroll}>
-              {blogPosts.map((post) => (
-                <div className="blog-card" key={post.id}>
-                  <h2>{post.title}</h2>
-                  <p>{post.content}</p>
-                </div>
-              ))}
-              {isLoading && <p>Loading...</p>}
-            </div>
-            <div className="blog-right">
-              <div className="recent-posts">
-                <h3>Recent Posts</h3>
-                <ul>
-                  {blogPosts.slice(0, 10).map((post) => (
-                    <li key={post.id}>
-                      <a href="#">{post.title}</a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="popular-topics">
-                <h3>Popular Topics</h3>
-                <ul>
-                  {/* Render up to 4 popular topics here */}
-                  {/* ... */}
-                </ul>
-              </div>
-              <div className="search">
-                <input type="text" placeholder="Search..." />
-                <button>Search</button>
-              </div>
-            </div>
-          </div>
-        </Layout>
-      )}
-    </>
+      </div>
+    </Layout>
   );
 };
 
