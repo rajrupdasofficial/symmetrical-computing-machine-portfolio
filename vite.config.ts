eb340e14e14e2facc6b2b7c499fa316342e08141
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-07 08:37:08 +0530

    Dotenv configuration added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 // https://vitejs.dev/config/
 export default defineConfig({
